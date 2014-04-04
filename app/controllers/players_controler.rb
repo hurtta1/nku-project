62cd1c54d643d2cb0d5c9f2ba0d5bc777ee83473
AlexHurtt <hurtta1@nku.edu>
2014-04-04 20:57:32 +0000

    added create and destroy methods to players controller

--- app/controllers/players_controler.rb
+++ app/controllers/players_controler.rb
@@ -1,4 +1,27 @@
 class PlayersController < ApplicationController
+  def new 
+    @player = Player.new
+  end
+  
+  def create
+    @player = Player.new(params[:student].permit(:name, :number, :email, :age))
+    if @player.save
+      redirect_to player_path, message: "Successfully signed up!."
+    end
+  end
+  
   def index
+    @player = Player.all
+  end
+
+  def destroy
+    @player = Player.find(params[:id])
+    @Player.destroy
+    redirect_to players_path
+  end
+
+ private  
+  def player_params
+    params.require(:player).permit(:name, :number, :age)
   end
 end
