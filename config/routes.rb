b6272148348d7a15bd81b0363dd592e9d0bebff5
AlexHurtt <hurtta1@nku.edu>
2014-04-05 03:12:15 +0000

    linked all current pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ NkuProject::Application.routes.draw do
   get "officials/index" => 'officials#index'
   get "teams/index" => 'teams#index'
   get "players/index" => 'players#index'
+  get "schedule/index" => 'schedule#index'
+  get "guidelines/index" => 'guidelines#index'
+  get "gallery/index" => 'gallery#index'
+  get "signup/index" => 'signup#index'
   resources :player, :team, :official, :church
   root 'home#index'
   end
